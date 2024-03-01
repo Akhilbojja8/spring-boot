07bcfc0bbc0994a093a4532c1b3c399dcf91184f
Akhilbojja8 <akhilbojja8@gmail.com>
2024-03-02 00:03:08 +0530

    first commit

---
+++ src/main/java/in/akhil/AdditionApp1Application.java
@@ -0,0 +1,15 @@
+package in.akhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionApp1Application.class, args);
+		
+		System.out.println("hello");
+	}
+
+}
